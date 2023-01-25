0cb8aea06ad743c05d21ff8b50dd021620fcc534
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2023-01-25 23:49:41 +0500

    Update index.js

--- Docker-Kubernetes-By-Laith-Harbs/Demo/index.js
+++ Docker-Kubernetes-By-Laith-Harbs/Demo/index.js
@@ -6,8 +6,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!!");
 });
 
-const PORT = 3000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 5000");
 });
